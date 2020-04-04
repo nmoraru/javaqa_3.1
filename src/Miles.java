e28198f09ed3827e0548d8e28b958a444009b2c0
nikita moraru <test@mail.com>
2020-04-04 18:49:08 +0300

    add feature Miles

---
+++ src/Miles.java
@@ -0,0 +1,7 @@
+public class Miles {
+    public static void main(String[] args) {
+        int ticketPriceInKopek = 2000199;
+        int updBonusMiles = ticketPriceInKopek / 2000;
+        System.out.println(updBonusMiles);
+    }
+}
